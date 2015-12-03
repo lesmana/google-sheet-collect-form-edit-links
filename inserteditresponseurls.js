f989c76b4670a2a95c3fb5ad3a0a28b308ff9698
Lesmana Zimmer <lesmana@gmx.de>
2015-12-03 04:55:23 +0100

    document functions

--- inserteditresponseurls.js
+++ inserteditresponseurls.js
@@ -6,6 +6,10 @@
  * http://www.wtfpl.net/ for more details.
  */
 
+/**
+ * add menu item when containing sheet is opened.
+ * can be run manually.
+ */
 function onOpen(e) {
   SpreadsheetApp.getUi()
       .createAddonMenu()
@@ -13,18 +17,38 @@ function onOpen(e) {
       .addToUi();
 }
 
+/**
+ * add menu item when installed as addon.
+ */
 function onInstall(e) {
   onOpen(e);
 }
 
+/**
+ * name of the sheet to be created for the edit response urls.
+ * if already exists sheet will be cleared.
+ */
 var sheetName = "Edit Response URLs";
 
+/**
+ * get form from spreadsheet.
+ * returns the form.
+ * does not check for errors.
+ * there is no getFormId().
+ * if there is use that instead.
+ * then use openById().
+ */
 function getForm(spreadsheet) {
   var formUrl = spreadsheet.getFormUrl();
   var form = FormApp.openByUrl(formUrl);
   return form;
 }
 
+/**
+ * create sheet or clear sheet if exists.
+ * returns the created or cleared sheet.
+ * does not check for errors.
+ */
 function getSheet(spreadsheet) {
   var sheet = spreadsheet.getSheetByName(sheetName);
   if (sheet != null) {
@@ -35,6 +59,10 @@ function getSheet(spreadsheet) {
   return sheet;
 }
 
+/**
+ * insert edit response urls from form to sheet.
+ * does not check for errors.
+ */
 function insertUrls(form, sheet) {
   var responses = form.getResponses();
   for (var i = 0; i < responses.length; i++) {
@@ -45,6 +73,15 @@ function insertUrls(form, sheet) {
   }
 }
 
+/**
+ * insert edit response urls from a linked form to a new sheet.
+ * sheet name controlled by variable named sheetName.
+ * sheet will be cleared if it already exists.
+ * does not check for errors.
+ * main function.
+ * called from the menu item added by onOpen().
+ * can be run manually.
+ */
 function insertEditResponseUrls() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var form = getForm(spreadsheet);
